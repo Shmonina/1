41de5b1f9b993237f2bcb8fc03728762ffac4c3a
Shmonina <114285113+Shmonina@users.noreply.github.com>
2022-11-06 18:47:49 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title> My page </title>
+<link rel="stylesheet" href="mycss.css"
+<link rel="shortcut icon" href="favicon.iso">
+</head>
+<body>
+<div class="mydiv1">
+<?php
+echo " Площадь трапеции с основаниями 4 и 5, высотой 3 равна ";
+echo 0.5*(4+5)*3;
+?>
+</div>
+</body>
+</html>
